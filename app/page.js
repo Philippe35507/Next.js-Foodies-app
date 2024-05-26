1b5894882aab22095ddd8f98fe8ff415070d5474
Philippe <55062877+Philippe35507@users.noreply.github.com>
2024-05-26 22:10:03 +0000

    Formatting
    Prettier

--- app/page.js
+++ app/page.js
@@ -1,16 +1,22 @@
 import Link from "next/link";
 
-Link
+Link;
 
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
+      <h1 style={{ color: "white", textAlign: "center" }}>
         Time to get started!
       </h1>
-      <p><Link href="/meals">Meals</Link></p>
-      <p><Link href="/meals/share">Share Meal</Link></p>
-      <p><Link href="/community">Community</Link></p>
+      <p>
+        <Link href="/meals">Meals</Link>
+      </p>
+      <p>
+        <Link href="/meals/share">Share Meal</Link>
+      </p>
+      <p>
+        <Link href="/community">Community</Link>
+      </p>
     </main>
   );
 }
--- components/main-header.js
+++ components/main-header.js
@@ -1,10 +1,24 @@
 import Link from "next/link";
 
-import logoImg from "@/assets/logo.png"
+import logoImg from "@/assets/logo.png";
 
-
-export default function MainHeader(){return <header>
-    <Link href="/"><img src={logoImg.src} alt="A plate with food on it"/>NextLevel Food</Link>
-
-    <nav><ul><il><Link href="/meals">Browse Meals</Link></il><il><Link href="/community">Foodies Community</Link></il></ul></nav>
-</header>}
+export default function MainHeader() {
+  return (
+    <header>
+      <Link href="/">
+        <img src={logoImg.src} alt="A plate with food on it" />
+        NextLevel Food
+      </Link>
+      <nav>
+        <ul>
+          <il>
+            <Link href="/meals">Browse Meals</Link>
+          </il>
+          <il>
+            <Link href="/community">Foodies Community</Link>
+          </il>
+        </ul>
+      </nav>
+    </header>
+  );
+}
