e388fd3a08e681b05b97cef6a6beccb4a4873343
Philippe <55062877+Philippe35507@users.noreply.github.com>
2024-05-29 21:33:05 +0000

    Active Links - The efficient way

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,34 +1,31 @@
-"use client"
 import Link from "next/link";
 
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
-import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
-const path = usePathname ()
-
   return (
-    <>    
-    <MainHeaderBackground />
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <Image src={logoImg} alt="A plate with food on it" priority />
-        NextLevel Food
-      </Link>
-      <nav className={classes.nav}>
-        <ul>
-          <il>
-            <Link href="/meals" className={path.startsWith("/meals") ? classes.active : undefined}>Browse Meals</Link>
-          </il>
-          <il>
-            <Link href="/community" className={path === "/community" ? classes.active : undefined}>Foodies Community</Link>
-          </il>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </>
   );
 }
